28f5dc175c4f0b5a731d52e370bc4e276e1cd8df
Spencer Florence <florence@ccs.neu.edu>
2014-12-24 14:42:25 -0600

    added monotone algorithm

--- test.rkt
+++ test.rkt
@@ -1,4 +1,6 @@
 #lang typed/racket
-(require "gif.rkt" "algos/gift-wrap.rkt" "algos/shared.rkt")
+(require "gif.rkt" "algos/gift-wrap.rkt" "algos/shared.rkt"
+         "algos/monotone.rkt")
 ;(debug #t)
-(render "/tmp/gif.gif" (random-data) gift-wrap)
+(render "/tmp/gif.gif" (random-data) #;gift-wrap
+        monotone)
