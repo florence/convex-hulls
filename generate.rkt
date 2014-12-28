7267972c3893d1769c2118f3507fa1477f166a15
Spencer Florence <florence@ccs.neu.edu>
2014-12-28 15:44:13 -0600

    generate now in a main

--- generate.rkt
+++ generate.rkt
@@ -1,19 +1,20 @@
 #!/usr/bin/env racket
 #lang racket
-(require "gif.rkt" "algos/gift-wrap.rkt" "algos/shared.rkt"
-         "algos/monotone.rkt" "algos/quickhull.rkt")
+(module+ main
+  (require "gif.rkt" "algos/gift-wrap.rkt" "algos/shared.rkt"
+           "algos/monotone.rkt" "algos/quickhull.rkt")
 
-(define file "/tmp/gif.rkt")
-(define v 
-  (command-line 
-   #:once-each
-   [("-f" "--file") f "file to output to. defaults to /tmp/gif.gif"
-    (set! file f)]
-   #:args (type) (string-downcase type)))
-(match v
-  [(or "g" "gift-wrap" "gift-wrap")
-   (render file (random-data) gift-wrap)]
-  [(or "m" "monotone")
-   (render file (random-data) monotone 20)]
-  [(or "q" "quickhull" "quick-hull")
-   (render file (random-data 200) quickhull 20)])
+  (define file "/tmp/gif.rkt")
+  (define v 
+    (command-line 
+     #:once-each
+     [("-f" "--file") f "file to output to. defaults to /tmp/gif.gif"
+      (set! file f)]
+     #:args (type) (string-downcase type)))
+  (match v
+    [(or "g" "gift-wrap" "gift-wrap")
+     (render file (random-data) gift-wrap)]
+    [(or "m" "monotone")
+     (render file (random-data) monotone 20)]
+    [(or "q" "quickhull" "quick-hull")
+     (render file (random-data 200) quickhull 20)]))
